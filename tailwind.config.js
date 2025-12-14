bd495d9755168901df000bf26170285614297622
tiendatmagic <tiendatmagic8@gmail.com>
2025-12-14 23:32:37 +0700

    update version

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,15 @@ export default {
   darkMode: 'class',
   content: [
     "./src/**/*.{html,ts}",
-    "./node_modules/flowbite/**/*.js" // add this line
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {
       boxShadow: {
         'card': '0 3px 6px -4px rgba(0,0,0,.12),0 6px 16px 0 rgba(0,0,0,.08),0 9px 28px 8px rgba(0,0,0,.05)',
+      },
+      colors: {
+        'dark-backdrop': '#000000'
       }
     },
   },
